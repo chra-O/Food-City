3a229dee981043154b56c9d2aab4de1dab26e263
Chra <chra.osman99@gmail.com>
2022-02-09 14:04:42 +0300

    adding login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import "./App.css";
+import LogInPage from "./Page/LogInPage";
 // import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+
     <>
+    <LogInPage />
   {/* <Routes>
       <Route path="invoices" element={<Invoices />}>
         <Route path=":invoiceId" element={<Invoice />} />
---
+++ src/Page/LogInPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LogInPage() {
+  return (
+    <>
+      <div className="flex justify-center w-screen h-screen bg-gray-100">
+        <div className="grid grid-cols-2 items-center " >
+          {" "}
+          <div className="">first</div>
+          <div className="">second</div>
+        </div>
+      </div>
+    </>
+  );
+}
