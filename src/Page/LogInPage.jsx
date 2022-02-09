1b10bc9aa84db8c953c54bf8f4598e7d7a9390e9
Chra <chra.osman99@gmail.com>
2022-02-09 18:01:50 +0300

    login

--- src/Page/LogInPage.jsx
+++ src/Page/LogInPage.jsx
@@ -3,11 +3,16 @@ import React from "react";
 export default function LogInPage() {
   return (
     <>
-      <div className="flex justify-center w-screen h-screen bg-gray-100">
-        <div className="grid grid-cols-2 items-center " >
+      <div className="flex justify-center w-screen h-screen bg-slate-50">
+        <div className="grid grid-cols-2 mt-20">
           {" "}
           <div className="">first</div>
-          <div className="">second</div>
+          <div className="">
+            <div className=" login  border border-gray-300 border-solid p-10 bg-white">
+              <h1 className="title">food city</h1>
+            </div>
+            <div className="signup mt-3  border border-gray-300 border-solid bg-white"></div>
+          </div>
         </div>
       </div>
     </>
