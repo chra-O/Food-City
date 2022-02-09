b06585bccbda53140fcd156e34610d9ab9faefaa
Chra <chra.osman99@gmail.com>
2022-02-09 13:38:51 +0300

    ading all freamwork and folder

---
+++ src/Redux/slice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const slice = createSlice({
+  name: "food",
+  initialState,
+  reducers: {}
+});
+
+export const {} = slice.actions
+
+export default slice.reducer
