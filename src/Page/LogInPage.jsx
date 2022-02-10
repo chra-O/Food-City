fc2d67365fb8f258222ac40936944eabfc50fcd4
Chra <chra.osman99@gmail.com>
2022-02-10 15:01:38 +0300

    adding logo

--- src/Page/LogInPage.jsx
+++ src/Page/LogInPage.jsx
@@ -8,9 +8,7 @@ export default function LogInPage() {
           {" "}
           <div className="">first</div>
           <div className="">
-            <div className=" login  border border-gray-300 border-solid p-10 bg-white">
-              <h1 className="title">food city</h1>
-            </div>
+            <div className=" login  border border-gray-300 border-solid p-10 bg-white"></div>
             <div className="signup mt-3  border border-gray-300 border-solid bg-white"></div>
           </div>
         </div>
