60043e9155fb864672c375d1051662d99296324e
Chra <chra.osman99@gmail.com>
2022-02-12 17:59:06 +0300

    header

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import LogInPage from "./Page/LogInPage";
 import { Routes, Route } from "react-router-dom";
 import ForgetPassword from "./Page/ForgetPassword";
 import SignUpPage from "./Page/SignUpPage";
+import Home from "./Page/Home";
 
 function App() {
   return (
     <>
-      {/* <LogInPage /> */}
-
       <Routes>
         <Route path="/" element={<LogInPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/ForgetPassword" element={<ForgetPassword />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
---
+++ src/Page/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="shadow-sm border-b bg-white w-full top-0 fixed">
+      <div className=" flex justify-between max-w-6xl mx-5 xl:mx-auto  p-2 ">
+        <h1 className=" relative foodLogin text-4xl mt-3 text-gray-700 ">
+          Food City
+        </h1>
+        <div className="relative  mt-1 p-1 pl-2 rounded-md  sm:text-sm  border-solid  border-[1.2px]  border-gray-300 flex bg-gray-50"></div>
+      </div>
+    </div>
+  );
+}
