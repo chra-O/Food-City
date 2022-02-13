09af17435c98dd850ff4d976760401f56870020e
Chra <chra.osman99@gmail.com>
2022-02-13 17:39:36 +0300

    change the tailwaid file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,27 +5,8 @@ module.exports = {
       fontSize: {
         xxs: ".60rem",
       },
-      animation: {
-        fade: 'fade 5s ease-in-out',
-      },
-      Keyframes: {
-        fade: {
-          '0%': {
-            opacity: 0,
-            left: '50%',
-            transform: 'translatex(-50%) scale(0%)'
-          },
-          '5%': { opacity: 1, left: '50%', transform: 'translatex(-50%)'},
-          '95%': { opacity: 1, left: '50%', transform: 'translatex(-50%)' },
-          '100%': {
-            opacity: 0,
-            left: '50%',
-            transform: 'translatex(-50%)scale(0)'
-          },
-        },
-      },
     },
   },
-  variants: { extend: {} },
+
   plugins: [],
 };
