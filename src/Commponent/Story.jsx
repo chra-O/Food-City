23940f84ec9bf88c8d08b5147586d6175f200fd6
Chra <chra.osman99@gmail.com>
2022-02-20 15:41:42 +0300

    adding story in home

---
+++ src/Commponent/Story.jsx
@@ -0,0 +1,133 @@
+import React, { useRef, useState } from "react";
+
+export default function Story() {
+  const storeRef = useRef(null);
+  const [left, setleft] = useState(false);
+  const [right, setRight] = useState(true);
+
+  const savedStory = [
+    {
+      id: "1",
+      username: "donat by chra",
+      img: "https://thekitchencommunity.org/wp-content/uploads/2021/03/43-Foods-That-Start-With-D-1200x900.jpg",
+    },
+    {
+      id: "2",
+      username: "oreng",
+      img: "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
+    },
+    {
+      id: "3",
+      username: "pasta",
+      img: "http://giovanniskitchen.com.au/staging/wp-content/uploads/2018/06/pasta.jpg",
+    },
+    {
+      id: "4",
+      username: "breakfast",
+      img: "https://static01.nyt.com/images/2021/02/17/dining/17tootired-grilled-cheese/17tootired-grilled-cheese-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    },
+    {
+      id: "5",
+      username: "birthday",
+      img: "https://static.toiimg.com/thumb/86814019.cms?width=680&height=512&imgsize=51588",
+    },
+    {
+      id: "6",
+      username: "fruit",
+      img: "https://post.healthline.com/wp-content/uploads/2018/06/fruit-732x549-thumbnail.jpg",
+    },
+    {
+      id: "7",
+      username: "cookes",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcgeQ1A_rVILPdzmQpJpWV-t4HlVjJqpMdKDSrtf5iBtrw6ZOJjRuG4WMSgUDk6xEE25A&usqp=CAU",
+    },
+    {
+      id: "8",
+      username: "pizza",
+      img: "https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg",
+    },
+    {
+      id: "9",
+      username: "sweet",
+      img: "https://www.efsa.europa.eu/sites/default/files/styles/share_opengraph/public/2021-03/experts_food_additives.jpg?h=827069f2&itok=Al_Mu6TK",
+    },
+  ];
+  const scrollHundlle = () => {
+    if (storeRef.current.scrollLeft > 0) {
+      setleft(true);
+    } else {
+      setleft(false);
+    }
+    if (
+      storeRef.current.scrollLeft ===
+      storeRef.current.scrollWidth -
+        storeRef.current.clientWidth +
+        0.44444274902344
+    ) {
+      setRight(false);
+    } else {
+      setRight(true);
+    }
+  };
+
+  return (
+    <>
+      {" "}
+      <div className=" ">
+        <div className=" relative  ">
+          <div
+            ref={storeRef}
+            onScroll={scrollHundlle}
+            className="  flex space-x-4 overflow-x-scroll max-w-5xl  p-2 scroll-smooth  scrollbar-hide  bg-white border "
+          >
+            {savedStory.map((story) => {
+              return (
+                <div className="  " key={story.id}>
+                  <div className="bg-gray-50 rounded-full   shrink-0 p-1 border border-yellow-400  w-16 h-16 ">
+                    <img
+                      src={story.img}
+                      className="rounded-full w-full h-full   "
+                      alt=""
+                    ></img>
+                  </div>
+                  <p className="  w-12 truncate text-center text-gray-700   font-semibold ">
+                    {story.username}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          <div className=" absolute  flex -top-1 md:-left-[25px] sm:-left-0  -left-2  w-full h-full justify-between   items-center">
+            <button
+              onClick={() => {
+                storeRef.current.scrollLeft = storeRef.current.scrollLeft - 300;
+              }}
+            >
+              <img
+                src="http://localhost:3000/image/left.png"
+                className={`    w-6 h-6 bg-white   cursor-pointer rounded-full  ${
+                  left ? `visible` : `invisible`
+                }
+             `}
+                alt=""
+              ></img>
+            </button>
+            <button
+              onClick={() => {
+                storeRef.current.scrollLeft = storeRef.current.scrollLeft + 300;
+              }}
+            >
+              <img
+                src="http://localhost:3000/image/right.png"
+                className={` w-6 h-6 bg-white   cursor-pointer rounded-full  ${
+                  right ? `visible` : `invisible`
+                }`}
+                alt=""
+              ></img>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../Commponent/NavBar";
+import Story from "../Commponent/Story";
 import "../Style/tip.css";
 
 export default function Home() {
@@ -11,213 +12,37 @@ export default function Home() {
         add="http://localhost:3000/image/add1.png"
         compasss="http://localhost:3000/image/compass1.png"
         not="http://localhost:3000/image/heart1.png"
-        
-
       />
-      <div className="mt-20 ">
-        to sed incidunt. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Itaque ducimus nemo molestiae officia suscipit illo sint fugit
-        cumque rerum, accusamus nihil, tenetur omnis deleniti molestias laborum
-        debitis architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. to sed incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus nemo
-        molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis del to sed incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Itaque ducimus incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. to sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. to sed incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus nemo
-        molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. to sed incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Itaque ducimus incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. to sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. to sed incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus nemo
-        molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. to sed incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Itaque ducimus incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. to sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. to sed incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus nemo
-        molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. to sed incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Itaque ducimus incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. to sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. to sed incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia incidunt. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus nemo
-        molestiae officia suscipit illo sint fugit cumque rerum, accusamus
-        nihil, tenetur omnis deleniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt.eniti molestias laborum debitis architecto sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. to sed incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Itaque ducimus incidunt.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
-        nemo molestiae officia incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        suscipit illo sint fugit cumque rerum, accusamus nihil, tenetur omnis
-        deleniti molestias laborum debitis architecto sed incidunt. to sed
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus incidunt. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Itaque ducimus nemo molestiae officia
-        incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque ducimus nemo molestiae officia suscipit illo sint fugit cumque
-        rerum, accusamus nihil, tenetur omnis deleniti molestias laborum debitis
-        architecto sed incidunt. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Itaque ducimus nemo molestiae officia suscipit illo
-        sint fugit cumque rerum, accusamus nihil, tenetur omnis deleniti
-        molestias laborum debitis architecto sed incidunt.
+      <div className="mt-10 ">
+        <div className="  pt-16 bg-gray-50  h-full pb-40  ">
+          <div className="sm:max-w-4xl  sm:mx-10 xl:mx-auto  mt-3  ">
+            <div className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3">
+              <div className="col-span-2  ">
+                {" "}
+                <div className="grid grid-cols-1 gap-3 mx-1">
+                 
+                    <Story/>
+                 
+                </div>
+              </div>
+              <div className="sm:invisible invisible  md:visible ">
+                <div className="flex ml-2 ">
+                  <img
+                    src="http://localhost:3000/image/chra.jpg"
+                    className=" sm:h-4 sm:w-4 md:h-14 md:w-14     bg-slate-500 rounded-full"
+                    alt=""
+                  ></img>
+                  <div className="grid grid-rows-2 ">
+                    {" "}
+                    <p className="  ml-4 text-black font-semibold  ">chraO </p>
+                    <p className=" ml-4 text-gray-500  ">chra othman</p>{" "}
+                  </div>
+                  <p className="flex mt-2 text-blue-500 ml-10">switch</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>{" "}
       </div>
     </>
   );
