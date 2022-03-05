468e6e6b95a35a0625398f686672e0f1839eb9e7
Chra <chra.osman99@gmail.com>
2022-03-05 13:52:32 +0300

    setting model done

--- src/Model/SettingModel.jsx
+++ src/Model/SettingModel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SettingModel() {
   return (
-    <div className="fixed z-20 top-48 left-[620px]">
+    <div className="fixed z-20 top-48 left-[75px] sm:left-[200px] lg:left-[620px]">
       <div className=" bg-gray-50 w-[450px] h-[500px] rounded-2xl   shadow-black   shadow-2xl border-2 grid grid-rows-6  ">
         <div className="border-b-2  h-14 flex justify-center items-center cursor-pointer">
           Change Password
@@ -25,7 +25,9 @@ export default function SettingModel() {
         <div className="border-b-2  h-14 flex justify-center items-center cursor-pointer">
           Report a Problem
         </div>
-        <div className="  h-14 flex justify-center items-center cursor-pointer">Log Out</div>
+        <div className="  h-14 flex justify-center items-center cursor-pointer">
+          Log Out
+        </div>
       </div>
     </div>
   );
